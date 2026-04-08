c029b3753251b3e3ca74bcd7e448a6e0bf45eef6
rupa-9777 <rupasriputta9777@gmail.com>
2026-04-08 18:39:08 +0530

    Problem 1: rename file and class

--- src/TransactionFeeSortingAuditCompliance.java
+++ src/TransactionFeeSortingAuditCompliance.java
@@ -12,7 +12,7 @@ class Transaction {
     }
 }
 
-public class TransactionFeeSortingAuditCompliance.java {
+public class TransactionFeeSortingAuditCompliance {
 
     static void bubbleSort(List<Transaction> list) {
         int n = list.size();
